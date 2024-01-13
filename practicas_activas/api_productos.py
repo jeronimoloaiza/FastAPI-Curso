54589cba0ecb46181adc49479bff40cfb50bcc79
jeronimoloaiza <jeronimo.loaiza@udea.edu.co>
2024-01-13 11:00:40 -0500

    Función para crear un producto

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -2,15 +2,15 @@ from typing import Optional
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-class Producto(BaseModel):
-    id: Optional[str]
+class ProductClass(BaseModel):
+    id: Optional[str] = None
     nombre: str
     precio_venta: float
     proveedor: str
 
 app = FastAPI()
 
-productos = []
+product_list = []
 
 @app.get('/')
 def index():
@@ -18,4 +18,9 @@ def index():
 
 @app.get('/producto')
 def obtener_productos():
-    return productos
+    return product_list
+
+@app.post('/producto')
+def crear_producto(new_product: ProductClass):
+    product_list.append(new_product)
+    return {'Mensaje': 'Producto creado satisfactoriamente'}
