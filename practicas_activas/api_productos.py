de39452303a19c5b98e4af33a742e5739ecafaf4
jeronimoloaiza <jeronimo.loaiza@udea.edu.co>
2024-01-13 10:48:48 -0500

    Clase para representar un producto

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -1,8 +1,16 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Producto(BaseModel):
+    id: Optional[str]
+    nombre: str
+    precio_venta: float
+    proveedor: str
 
 app = FastAPI()
 
-
+productos = []
 
 @app.get('/')
 def index():
