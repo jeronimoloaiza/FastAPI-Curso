fe1a819f61255decd71884e29d7e1603b703e2ff
jeronimoloaiza <jeronimo.loaiza@udea.edu.co>
2024-01-13 15:21:39 -0500

    Ruta para eliminar un producto a partir de su ID

--- practicas_activas/api_productos.py
+++ practicas_activas/api_productos.py
@@ -17,9 +17,11 @@ product_list = []
 def index():
     return {'Mensaje' : 'Bienvenidos a la API de Productos'}
 
+
 @app.get('/producto')
 def obtener_productos():
     return product_list
+
 
 @app.post('/producto')
 def crear_producto(new_product: ProductClass):
@@ -27,14 +29,28 @@ def crear_producto(new_product: ProductClass):
     product_list.append(new_product)
     return {'Mensaje': 'Producto creado satisfactoriamente'}
 
+
 @app.get('/producto/{product_id}')
 def obtener_producto_por_id(product_id: str):
 #    for p in product_list:
 #        if p.id == product_id:
-#            return p
+#            return pfrom http.client import 
     resultado = list(filter(lambda p: p.id == product_id, product_list ))
 
     if len(resultado): #Lo mismo que if len(resultado) > 0:
         return resultado
 
+    raise HTTPException(status_code=404, detail=f'El producto con la id {product_id} no fue encontrado')
+
+
+@app.delete('/producto/{product_id}')
+def eliminar_profucto_por_id(product_id:str):
+    resultado = list(filter(lambda p: p.id == product_id, product_list ))
+
+    if len(resultado):
+        producto = resultado[0]
+        product_list.remove(producto)
+
+        return {'Mensaje' : f'El producto con la id {product_id} fue eliminado'}
+    
     raise HTTPException(status_code=404, detail=f'El producto con la id {product_id} no fue encontrado')
